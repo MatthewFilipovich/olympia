722f43ff7b0dd3ba27e9c55dc6fc4a9f3806e458
Matthew Filipovich <mjfilipovi@gmail.com>
2019-11-26 18:29:17 -0500

    Update jupyter

--- emergent_soccer/__init__.py
+++ emergent_soccer/__init__.py
@@ -1,6 +1,11 @@
 from gym.envs.registration import register
 
 register(
-    id='soccer-v0',
+    id='soccer-RBG',
+    entry_point='emergent_soccer.envs:FieldEnv',
+)
+
+register(
+    id='soccer-RAM',
     entry_point='emergent_soccer.envs:FieldEnv',
 )
