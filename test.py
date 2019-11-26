034ed9c75037531d83903a90bd222294546913c4
Hugh Morison <hugh.morison@hotmail.ca>
2019-11-26 18:28:06 -0500

    rgb + ram outputs

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import olympia
 
 class TestStringMethods(unittest.TestCase):
     def test_moving_throwing(self):
-        env = gym.make('soccer-v0')
+        env = gym.make('olympia-rgb')
         env.render()
         moves = [5,5,5,5,5,5,5,13,5,5,5,5,5,5,1,1,1,13,0]
         ep = []
