a414b2a7d773298dd77009260a2742eebccd0c8c
Hugh Morison <hugh.morison@hotmail.ca>
2019-11-26 23:37:44 -0500

    one v one training

--- olympia/envs/training_schemes.py
+++ olympia/envs/training_schemes.py
@@ -13,5 +13,5 @@ This file could have different parameters for training schemes hardcoded
 
 scheme = {
     "one player": [[(0.7, 0.5)]],
-    "one v one": [[()],[()]]
+    "one v one": [[(0.7, 0.5)], [(0.3, 0.5)]]
 }
