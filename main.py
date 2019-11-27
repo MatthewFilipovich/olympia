ac0a07ae909c53829a11d827702bc4d246f6aeab
Matthew Filipovich <mjfilipovi@gmail.com>
2019-11-27 00:14:58 -0500

    Fix main file

--- main.py
+++ main.py
@@ -1,20 +1,12 @@
-import numpy as np
-import random
-import gym
-from collections import deque
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.optimizers import Adam
 from olympia.envs import OlympiaRGB, OlympiaRAM
-import time
 
 if __name__ == "__main__":
     shape = (21, 15)
     training_level = 'one v one'
-    train_episodes = 1000
     batch_size = 25
+    train_episodes = 1000
     run_episodes = 3
 
-    env = OlympiaRGB(shape=shape, training_level=training_level)
-    env.train(episodes, batch_size, render=False, load_saved=False)
-    env.run(episodes,render=True)
+    env = OlympiaRAM(shape=shape, training_level=training_level)
+    env.train(episodes=train_episodes, batch_size=batch_size, render=True, load_saved=False)
+    env.run(episodes=run_episodes,render=True)
