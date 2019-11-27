7824b14614901daf66d441c4828358a59c070a83
Matthew Filipovich <mjfilipovi@gmail.com>
2019-11-26 23:59:20 -0500

    Add train function to environment

--- main.py
+++ main.py
@@ -9,26 +9,11 @@ from olympia.envs import OlympiaRGB, OlympiaRAM
 import time
 
 if __name__ == "__main__":
-    EPISODES = 1000
     shape = (21, 15)
-    training_level = 'one player'
+    training_level = 'one v one'
+    episodes = 1000
+    batch_size = 25
+
     env = OlympiaRGB(shape=shape, training_level=training_level)
-    agents = env.get_agents()
-    # agent.load("soccer.h5")
-    done = False
-    batch_size = 32
-    start_time = time.time()
-    for e in range(EPISODES):
-        state = env.reset()
-        while not done:
-            env.render()
-            actions = [agent.choose_action(state) for agent in agents]
-            next_state, rewards, done, _ = env.step(*actions)
-            for agent, action, reward in zip(agents, actions, rewards):       
-                agent.remember(state, action, reward, next_state, done)
-                if len(agent.memory) > batch_size and not done:
-                    agent.replay(batch_size)
-            if done:
-                print("Episode {}/{} complete. Training time: {}"
-                      .format(e, EPISODES, time.time() - start_time))
-            state = next_state
+    env.train(episodes, batch_size, render=False, load_saved=False)
+    env.run(render=True)
